8af0b23708f2b80c2208f66912703c59fc098e86
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-17 21:44:11 +0300

    36. Defining Tools for our ReAct agent

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
 from dotenv import load_dotenv
+from langchain.tools import tool
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import render_text_description
+from langchain_openai import ChatOpenAI
+
 load_dotenv()
 
-def get_text_length(text:str) -> int:
+
+@tool
+def get_text_length(text: str) -> int:
     """Returns the length of a atext by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("\n").strip('"')  # stripping away non alphabetical characters
     return len(text)
 
-if __name__ == '__main__':
-    print("The length is: ")
-    print(get_text_length(text="Dog"))
+
+if __name__ == "__main__":
+    tools = [get_text_length]
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools = render_text_description(tools), tool_names=".".join(t.name for t in tools)
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
