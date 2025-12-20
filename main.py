02a504064dd3b71ba2ff52e2baceac4bdb507012
ytrofimov <Yuriy.Trofimov@aplana.com>
2025-12-20 12:35:33 +0300

    38. ReAct prompt, LLM Reasoning Engine, Output Parsing and Tool Execution

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from typing import Union, List
+
 from dotenv import load_dotenv
 from langchain.tools import tool
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import render_text_description
+from langchain_core.tools import render_text_description, Tool
 from langchain_openai import ChatOpenAI
-
+from langchain_classic.agents.output_parsers import ReActSingleInputOutputParser
 load_dotenv()
 
 
@@ -14,33 +17,49 @@ def get_text_length(text: str) -> int:
     text = text.strip("\n").strip('"')  # stripping away non alphabetical characters
     return len(text)
 
+def find_tool_by_name(tools: List[Tool], tool_name:str)-> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+        raise ValueError(f"Tool with name {tool_name} not found")
 
 if __name__ == "__main__":
     tools = [get_text_length]
     template = """
     Answer the following questions as best you can. You have access to the following tools:
 
-    {tools}
-    
-    Use the following format:
-    
-    Question: the input question you must answer
-    Thought: you should always think about what to do
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ... (this Thought/Action/Action Input/Observation can repeat N times)
-    Thought: I now know the final answer
-    Final Answer: the final answer to the original input question
-    
-    Begin!
-    
-    Question: {input}
-    Thought:
-    """
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+Thought:"""
 
     prompt = PromptTemplate.from_template(template=template).partial(
         tools = render_text_description(tools), tool_names=".".join(t.name for t in tools)
     )
 
     llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
+    agent = {"input": lambda x:x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of DOG in characters?"})
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
